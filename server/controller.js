73f6ab13c9975383214e07ea33414b00d3fbd2e2
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-10-20 13:29:05 -0700

    moved rollbar to controller

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,17 @@
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'd70a3f2e6fca4900b8a20eb7567bed3f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('This website is up and running!')
+
+
 const fortunes = ["Happiness begins with facing life with a smile and a wink.", "Happy life is just in front of you.", "He who expects no gratitude shall never be disappointed.", "If a true sense of value is to be yours it must come through service."];
 
 const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
@@ -18,6 +30,7 @@ module.exports = {
     getCompliment: (req, res) => {
               
         // choose random compliment
+        rollbar.log('compliment was gotten')
         let randomIndex = Math.floor(Math.random() * compliments.length);
         let randomCompliment = compliments[randomIndex];
         
--- server/index.js
+++ server/index.js
@@ -6,17 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: 'd70a3f2e6fca4900b8a20eb7567bed3f',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
-
-// record a generic message and send it to Rollbar
-rollbar.log('This website is up and running!')
-
 const { getHTML, getJS } = require('./controller')
 
 app.get('/', getHTML);
